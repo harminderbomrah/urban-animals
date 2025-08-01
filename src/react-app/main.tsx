45072bf60dc3ff3a46d7aaf4067d14f6b20d7132
Bomrah Harry <harry@funpodium.net>
2025-08-01 21:43:24 +0800

    fi

--- src/react-app/main.tsx
+++ src/react-app/main.tsx
@@ -1,6 +1,6 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import "@fontsource/eb-garamond";
+import "@fontsource/eb-garamond/index.css";
 import "./index.css";
 import App from "./App.tsx";
 
